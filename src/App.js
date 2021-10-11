845adc565da587bec440d2fe21f495fe124705bd
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-10-11 18:26:10 -0500

    Add header with navigation menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // <Provider store={configureStore}>
+    <Router>
+      <Header />
+      <Switch>
+        {/* <Route path="" exact component={Home}></Route>
+        <Route path="" component={CategoriesPage}></Route> */}
+      </Switch>
+    </Router>
+  // </Provider>
   );
 }
 
