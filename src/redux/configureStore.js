b69b9045aea0fb6ecb83cd5d835a0ca2c7431a4d
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-10-12 20:46:47 -0300

    Fix incompatibility

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import rocketsReducer from './Rockets/Rockets-Reducer';
-import profileReducer from './Profile/Profile-Reducer';
-import missionsReducer from './Missions/Missions-Reducer';
+// import rocketsReducer from './Rockets/Rockets-Reducer';
+// import profileReducer from './Profile/Profile-Reducer';
+import missionsReducer from './missions/missions';
 
 const reducer = combineReducers({
-  rocketsReducer,
-  profileReducer,
-  missionsReducer,
+  // rocketsReducer,
+  // profileReducer,
+  missions: missionsReducer,
   // additional reducers could be added here
 });
 
