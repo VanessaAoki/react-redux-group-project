6781e51339986ba4c23784976c6a5c9020442892
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-10-12 15:42:13 -0500

    Active page will style on links, add ducs structure folders

---
+++ src/redux/Rockets/Rockets-Reducer.js
@@ -0,0 +1,29 @@
+// Some default code
+
+// Actions
+
+const initialState = [];
+
+// Reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 1: {
+      return (
+        newState
+      );
+    }
+
+    case 2: {
+      return newState;
+    }
+    case 3: {
+      return action.state;
+    }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
+
+// Action Creators
