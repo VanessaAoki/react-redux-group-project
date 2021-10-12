304d98eec3b10bcc14cb6dcda8caa47067618d2d
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-10-11 22:44:49 -0300

    Add profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import './Profile.css';
 
 const Profile = () => (
-  <h2>Profile</h2>
+  <div className="profile-container">
+    <section className="profile-missions">
+      <h2>My Missions</h2>
+      <ul className="profile-table">
+        <li>Falcon 9</li>
+        <li>Falcon 9</li>
+        <li>Falcon 9</li>
+      </ul>
+    </section>
+    <section className="profile-rockets">
+      <h2>My Rockets</h2>
+      <ul className="profile-table">
+        <li>Telstar</li>
+        <li>Telstar</li>
+        <li>Telstar</li>
+      </ul>
+    </section>
+  </div>
 );
 
 export default Profile;
