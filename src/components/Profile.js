fc44560f9319a6a9381a037d5499349c7c4a1a25
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-10-13 12:28:04 -0300

    Add joined missions to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,25 +1,39 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './Profile.css';
 
-const Profile = () => (
-  <div className="profile-container">
-    <section className="profile-missions">
-      <h2>My Missions</h2>
-      <ul className="profile-table">
-        <li>Falcon 9</li>
-        <li>Falcon 9</li>
-        <li>Falcon 9</li>
-      </ul>
-    </section>
-    <section className="profile-rockets">
-      <h2>My Rockets</h2>
-      <ul className="profile-table">
-        <li>Telstar</li>
-        <li>Telstar</li>
-        <li>Telstar</li>
-      </ul>
-    </section>
-  </div>
-);
+const Profile = () => {
+  const missions = useSelector((state) => state.missions);
+
+  const joinedMissions = missions.filter((mission) => {
+    if (mission.status === true) {
+      return mission;
+    }
+    return null;
+  });
+
+  return (
+    <div className="profile-container">
+      <section className="profile-missions">
+        <h2>My Missions</h2>
+        <ul className="profile-table">
+          {joinedMissions.map((mission) => (
+            <li key={mission.mission_id}>
+              {mission.mission_name}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className="profile-rockets">
+        <h2>My Rockets</h2>
+        <ul className="profile-table">
+          <li>Telstar</li>
+          <li>Telstar</li>
+          <li>Telstar</li>
+        </ul>
+      </section>
+    </div>
+  );
+};
 
 export default Profile;
